f9415f27d0cfde50fde58d205c2e909e95a5b33a
cdinuta <cdinuta@axway.com>
2019-11-14 07:48:42 +0200

    testing disaster recovery for only 1 eureka server

--- tests/eureka_client_replication_test.py
+++ tests/eureka_client_replication_test.py
@@ -66,6 +66,25 @@ class FlaskServerTestCase(unittest.TestCase):
         self.assertEqual(len(apps_list2), 1)
         self.assertEqual(apps_list1, apps_list2)
 
+    def test_eureka_replication_just_one_up_and_recovers(self):
+        DockerUtils.up_service(self.file, "eureka-server1")
+        time.sleep(self.time_to_wait_until_compose_up)
+        DockerUtils.up_service(self.file, "estuary-discovery")
+        time.sleep(self.time_to_wait_until_discovery_up)
+        apps_list1 = EurekaClient("http://localhost:8080/eureka/v2").get_apps()
+        self.assertEqual(len(apps_list1), 1)
+        try:
+            apps_list2 = EurekaClient("http://localhost:8081/eureka/v2").get_apps()
+        except Exception as e:
+            self.assertIsInstance(e, URLError)
+
+        DockerUtils.stop_service(self.file, "eureka-server1")
+        time.sleep(self.time_to_wait_until_compose_up)
+        DockerUtils.up_service(self.file, "eureka-server1")
+        time.sleep(self.time_to_wait_until_compose_up)
+        apps_list1 = EurekaClient("http://localhost:8080/eureka/v2").get_apps()
+        self.assertEqual(len(apps_list1), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
