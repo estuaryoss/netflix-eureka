371c8b2fe5bd368e6f603e5340979407dadb3ce2
Catalin Dinuta <constantin.dinuta@gmail.com>
2020-10-15 15:04:07 +0300

    updated image location

--- docker_push_master.sh
+++ docker_push_master.sh
@@ -5,5 +5,5 @@ echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 VERSION=`cat version.txt`
 echo "build eureka version ${VERSION}"
 
-docker build . --build-arg EUREKA_VERSION="${VERSION}" -t estuaryoss/netflix-eureka:latest
-docker push estuaryoss/netflix-eureka:latest
+docker build . --build-arg EUREKA_VERSION="${VERSION}" -t dinutac/netflixoss-eureka:latest
+docker push dinutac/netflixoss-eureka:latest
